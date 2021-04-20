2a8448d407eecdc093ae3df1cbf769bd58563ed8
nyatender <negi.yatenn@gmail.com>
2021-04-20 20:46:06 +0530

    javascript programs

---
+++ javaScriptCode.js
@@ -0,0 +1,10 @@
+
+function toCelsius() {
+  // if(true)
+  // {
+  //   return "in";
+  // }
+  return (5/9) * (32);
+}
+document.getElementById("demo").innerHTML = toCelcius();
+//console.log(toCelsius());
